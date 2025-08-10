d56cb175ad02aff59c3476010899e09c54d32f84
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2025-08-10 13:01:42 +0200

    Base

--- backend/profiles/apps.py
+++ backend/profiles/apps.py
@@ -1,6 +1,8 @@
+# Django import:
 from django.apps import AppConfig
 
 
+# Profiles application configuration:
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'

80a0f27066f42eb567d4762fc1d4eeba4b402710
robertrkkr <robert.kucharski.rkkr@gmail.com>
2025-08-28 18:00:30 +0200

    Fix

--- backend/alpenwegs/ashared/api/mixins/base_list_mixin.py
+++ backend/alpenwegs/ashared/api/mixins/base_list_mixin.py
@@ -1,11 +1,14 @@
 # AlpenWegs import:
+from alpenwegs.ashared.models.creator_model import BaseCreatorModel
 from alpenwegs.ashared.api.mixins.base_mixin import BaseMixin
 
 # Rest framework import:
 from rest_framework.mixins import ListModelMixin
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
-from rest_framework import status
+
+# Django import:
+from django.db import models
 
 
 class BaseListModelMixin(
@@ -16,10 +19,61 @@ class BaseListModelMixin(
     Base model mixin for listing a queryset.
     """
 
-    def _call_list(self):
+    # Define custom list values:
+    allow_list = False
+
+    def _filter_for_user(self,
+        queryset
+    ):
+        """
+        Apply ownership/public filtering if model inherits BaseCreatorModel.
+        """
+        
+        # Get model class from queryset:
+        model_cls = getattr(queryset, 'model', None)
+
+        # Define check if model is instance of BaseCreatorModel:
+        is_base_creator_model = False
+
+        # Check if model is instance of BaseCreatorModel: 
+        if model_cls and issubclass(model_cls, BaseCreatorModel):
+
+            # Mark as BaseCreatorModel:
+            is_base_creator_model = True
+            
+            # Collect user from request:
+            user = self.request.user
+            if user and user.is_authenticated:
+                # Run filtering for authenticated user:
+                return queryset.filter(
+                    models.Q(creator=user) | models.Q(is_public=True)
+                )
+            else:
+                # Run filtering for unauthenticated user:
+                return queryset.filter(
+                    is_public=True
+                )
+
+        # Return original queryset if no filtering applied:
+        return queryset, is_base_creator_model
+
+    def _call_list(self,
+        request: Response,
+        *args: list,
+        **kwargs: dict,
+    ) -> Response:
 
         # Collect all instances:
         queryset = self.filter_queryset(self.get_queryset())
+
+        # # Apply ownership / visibility filtering on queryset:
+        # queryset, is_base_creator_model = self._filter_for_user(
+        #     queryset,
+        # )
+
+        # # Apply allow_list flag:
+        # if not is_base_creator_model and self.allow_list is False:
+        #     return []
         
         # Prepare page view with pagination:
         page = self.paginate_queryset(queryset)
@@ -33,11 +87,19 @@ class BaseListModelMixin(
         serializer = self.get_serializer(queryset, many=True)
         return serializer.data
 
-    def list(self, request, *args, **kwargs):
+    def list(self,
+        request: Response,
+        *args: list,
+        **kwargs: dict,
+    ) -> Response:
 
         try:
             # Run a new API GET List call:
-            response = self._call_list()
+            response = self._call_list(
+                request=request,
+                *args,
+                **kwargs,
+            )
 
         except Exception:
             # Raise not found error:

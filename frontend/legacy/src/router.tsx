b79c967f4965eca024980756292996cae946b1c9
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2025-09-14 22:06:44 +0200

    Frontend test

---
+++ frontend/legacy/src/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import App from "./App"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/login", element: <LoginPage /> },
+    ],
+  },
+])
+
+export default function AppRouter() {
+  return <RouterProvider router={router} />
+}

110818bc5e70dbd2243cb27d02fe20972b46f4c3
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2025-12-14 21:12:59 +0100

    Footer and 404 updated

--- frontend/src/app/pages/NotFoundPage.tsx
+++ frontend/src/app/pages/NotFoundPage.tsx
@@ -10,9 +10,7 @@ export function NotFoundPage() {
     <div className="box-component-pagination not-found-page">
       <ShieldAlert className="not-found-icon" />
       <h2 className="not-found-title">404</h2>
-      <p className="not-found-description">
-        The page you are looking for does not exist.
-      </p>
+      <p className="not-found-description">The page you are looking for does not exist.</p>
       <NavLink to="/" style={{ textDecoration: "underline" }} className="not-found-link">
         Go back to Home
       </NavLink>
